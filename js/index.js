21e978262dd441f88889e3ce3ac4751042fd7284
SubashAravindan <p.subasharavindan@gmail.com>
2019-02-08 01:49:13 +0530

    Change grab line color to yellow

--- js/index.js
+++ js/index.js
@@ -50,7 +50,7 @@ particlesJS('particles-js',
       'line_linked': {
         'enable': false,
         'distance': 150,
-        'color': '#ffffff',
+        'color': '#d5d819',
         'opacity': 0.4,
         'width': 1
       },
